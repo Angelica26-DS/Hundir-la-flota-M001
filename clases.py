82f5baff4abf962f148d30073ac399f0dcb34211
hugosanz1222-cloud <hugosanz1222@gmail.com>
2026-03-30 13:05:22 +0200

    Aqui he definido las clases en funcion de lo que me tocaba esta semana como tarea

---
+++ clases.py
@@ -0,0 +1,15 @@
+import numpy as np
+from variables import FILAS, COLUMNAS, AGUA
+
+class Tablero:
+    def __init__(self, jugador):
+        self.jugador = jugador
+        self.tablero = np.full((FILAS, COLUMNAS), AGUA)
+
+    def mostrar_tablero(self):
+        print(self.tablero)
+    
+tablero1 = Tablero("Hugo")
+tablero1.mostrar_tablero()
+
+
