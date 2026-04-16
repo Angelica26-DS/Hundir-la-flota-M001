e52b840c0509ce26c77986f2bd764cc04979eb67
hugosanz1222-cloud <hugosanz1222@gmail.com>
2026-04-16 18:29:45 +0200

    Update clases.py

--- clases.py
+++ clases.py
@@ -1,12 +1,16 @@
 import numpy as np
 from variables import FILAS, COLUMNAS, AGUA
 
+
 class Tablero:
     def __init__(self, jugador):
+        #Guardamos el nombre del jugador al que pertenece este tablero
         self.jugador = jugador
+        #Creamos la matriz del tablero con numpy, rellenada completamente con AGUA
         self.tablero = np.full((FILAS, COLUMNAS), AGUA)
 
     def mostrar_tablero(self):
+        # Imprimimos por consola el estado actual del tablero
         print(self.tablero)
 
 
